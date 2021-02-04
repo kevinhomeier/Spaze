be54d57edf73c08f441e5f2be8d60ed44f3ec19b
ChrisWalker5120 <78027886+ChrisWalker5120@users.noreply.github.com>
2021-02-04 17:19:01 +0100

    PlayerAnimation

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -6,18 +6,18 @@ public class Playermovement : MonoBehaviour
 {
     public float movespeed;
     public Rigidbody2D rb;
-    Vector2 movement;
     public Animator animator;
 
     void Update()
     {
-        animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+
+        transform.position = transform.position + movement  * Time.deltaTime;
     }
 
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
-    }
 }
