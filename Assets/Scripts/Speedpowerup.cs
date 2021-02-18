13b8e9abb8c853f125b36a1af73c6ec7573d9684
kevinhomeier <kevin.homeier7201@gmail.com>
2021-02-18 16:34:31 +0100

    ive commited

--- Assets/Scripts/Speedpowerup.cs
+++ Assets/Scripts/Speedpowerup.cs
@@ -7,9 +7,11 @@ public class Speedpowerup : MonoBehaviour
     public float timedpowerup = 4f;
     public float multiplier = 2f;
     public bool speed = false;
+    public float timer;
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        timer = 5f;
         if (other.CompareTag("Player"))
         {
             StartCoroutine(Pickup(other));
@@ -20,13 +22,29 @@ public class Speedpowerup : MonoBehaviour
     IEnumerator Pickup(Collider2D player)
     {
         
-        Playermovement playerscript = player.GetComponent<Playermovement>();
+        PlayerLvl3 playerscript = player.GetComponent<PlayerLvl3>();
         playerscript.movespeed *= multiplier;
         GetComponent<SpriteRenderer>().enabled = false;
         GetComponent<Collider2D>().enabled = false;
         yield return new WaitForSeconds(timedpowerup);
         playerscript.movespeed /= multiplier;
-        Destroy(gameObject);
+        
         speed = false;
+
+    }
+
+    private void Update()
+    {
+
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                GetComponent<SpriteRenderer>().enabled = true;
+                GetComponent<Collider2D>().enabled = true;
+
+            }
+        }
     }
 }
